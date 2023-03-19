937d5f598202e4088a1bf16a5d1199eb1180ec7c
riazul islam <riazulislam5280@gmail.com>
2023-03-19 18:41:55 +0600

    routing system changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Users from './pages/Users';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <div className="py-[30px] px-[80px]">
-      <Outlet />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,52 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
 import './index.css';
 import App from './App';
-
-// pages
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Users from './pages/Users';
-import Dashboard from './pages/Dashboard';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Dashboard />
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />
-      },
-      {
-        path: "/users",
-        element: <Users />
-      },
-      {
-        path: "/signin",
-        element: <SignIn />
-      },
-      {
-        path: "/signup",
-        element: <SignUp />
-      }
-    ]
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
